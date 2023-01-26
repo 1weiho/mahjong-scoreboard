f5ceb8673918ad32f69214ecba68683360847541
David Ho <davidho0403@gmail.com>
2023-01-27 01:50:20 +0800

    feat: init player name feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react"
+import InitPlayer from "./InitPlayer"
+import { Player } from "./Player"
+import { GameData } from "./GameData"
+
 function App() {
-  return <div>App</div>
+  const [gameData, setGameData] = useState<GameData>()
+  const [players, setPlayers] = useState<Player[]>([])
+
+  // Check if the player info is in the local storage first
+  useEffect(() => {
+    if ("gameData" in localStorage) {
+      const gameDataLocal = JSON.parse(localStorage.getItem("gameData") as string) as GameData
+      setPlayers(gameDataLocal.players)
+    }
+  }, [])
+
+  // Sync the gameData between state and localStorage when gameData changed
+  useEffect(() => {
+    localStorage.setItem("gameData", JSON.stringify(gameData))
+    return () => {
+      localStorage.removeItem("gameData")
+    }
+  }, [gameData])
+
+  // Save player data to gameData when players changes
+  useEffect(() => {
+    setGameData({ ...gameData, players: players })
+  }, [players])
+
+  return (
+    <div className="h-screen w-screen bg-slate-100 flex justify-center items-center">
+      {players.length == 0 && <InitPlayer setPlayers={setPlayers} />}
+    </div>
+  )
 }
 
 export default App
---
+++ src/InitPlayer.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react"
+import { Player } from "./Player"
+
+interface Props {
+  setPlayers: (newPlayer: Player[]) => void
+}
+
+function InitPlayer({ setPlayers }: Props) {
+  const [player1, setPlayer1] = useState<Player>({ name: "", wind: "East", score: 0 })
+  const [player2, setPlayer2] = useState<Player>({ name: "", wind: "West", score: 0 })
+  const [player3, setPlayer3] = useState<Player>({ name: "", wind: "South", score: 0 })
+  const [player4, setPlayer4] = useState<Player>({ name: "", wind: "North", score: 0 })
+
+  return (
+    <form
+      className="h-1/2 w-1/4 rounded-lg shadow-md bg-white flex flex-col px-12 space-y-8 items-center"
+      onSubmit={() => setPlayers([player1, player2, player3, player4])}
+    >
+      <h1 className="text-center text-2xl tracking-widest text-slate-900 mt-12">初始化玩家</h1>
+      <input
+        type="text"
+        placeholder="東 - 玩家 1"
+        className="border-2 border-slate-200 rounded-md p-3 w-full"
+        onChange={(e) => setPlayer1((prevState) => ({ ...prevState, name: e.target.value }))}
+        required
+      />
+      <input
+        type="text"
+        placeholder="西 - 玩家 2"
+        className="border-2 border-slate-200 rounded-md p-3 w-full"
+        onChange={(e) => setPlayer2((prevState) => ({ ...prevState, name: e.target.value }))}
+        required
+      />
+      <input
+        type="text"
+        placeholder="南 - 玩家 3"
+        className="border-2 border-slate-200 rounded-md p-3 w-full"
+        onChange={(e) => setPlayer3((prevState) => ({ ...prevState, name: e.target.value }))}
+        required
+      />
+      <input
+        type="text"
+        placeholder="北 - 玩家 4"
+        className="border-2 border-slate-200 rounded-md p-3 w-full"
+        onChange={(e) => setPlayer4((prevState) => ({ ...prevState, name: e.target.value }))}
+        required
+      />
+      <button
+        className="border-cyan-700 border-2 rounded-lg px-3 py-2 w-24 text-cyan-700 hover:bg-cyan-700 hover:text-white duration-150 tracking-widest"
+        type="submit"
+      >
+        繼續
+      </button>
+    </form>
+  )
+}
+
+export default InitPlayer
