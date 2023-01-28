db63dac1dcd85662547b01eef6073d3365118a85
David Ho <davidho0403@gmail.com>
2023-01-29 03:34:52 +0800

    feat: set player wind layout match mahjong rule

--- src/pages/Gaming.tsx
+++ src/pages/Gaming.tsx
@@ -23,28 +23,28 @@ const Gaming = () => {
         <div className="grid grid-rows-3 h-full w-full auto-rows-auto">
           <div className="row-span-1 flex justify-center items-start">
             <PlayerCard
-              wind={gameData?.players[0].wind}
-              name={gameData?.players[0].name}
-              score={gameData?.players[0].score}
-            />
-          </div>
-          <div className="row-span-1 flex justify-between items-center">
-            <PlayerCard
-              wind={gameData?.players[1].wind}
-              name={gameData?.players[1].name}
-              score={gameData?.players[1].score}
-            />
-            <PlayerCard
               wind={gameData?.players[2].wind}
               name={gameData?.players[2].name}
               score={gameData?.players[2].score}
             />
           </div>
-          <div className="row-span-1 flex justify-center items-end">
+          <div className="row-span-1 flex justify-between items-center">
             <PlayerCard
               wind={gameData?.players[3].wind}
               name={gameData?.players[3].name}
               score={gameData?.players[3].score}
+            />
+            <PlayerCard
+              wind={gameData?.players[1].wind}
+              name={gameData?.players[1].name}
+              score={gameData?.players[1].score}
+            />
+          </div>
+          <div className="row-span-1 flex justify-center items-end">
+            <PlayerCard
+              wind={gameData?.players[0].wind}
+              name={gameData?.players[0].name}
+              score={gameData?.players[0].score}
             />
           </div>
         </div>
