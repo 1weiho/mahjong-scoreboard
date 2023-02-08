8f6593280f4129d44603b8e2f1aca0cad6a40846
David Ho <davidho0403@gmail.com>
2023-02-08 16:45:34 +0800

    feat: remove github pages deploy config

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="/mahjong-scoreboard/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
